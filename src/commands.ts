095447268f706154d7de710ad864bae1e9973474
Martin-Stach <dev@martinstach.de>
2025-12-02 23:13:40 +0100

    adds exit and help command

---
+++ src/commands.ts
@@ -0,0 +1,18 @@
+import type { CLICommand } from "./command.js";
+import { commandExit } from "./command_exit.js";
+import { commandHelp } from "./command_help.js";
+
+export function getCommands(): Record<string, CLICommand> {
+  return {
+    exit: {
+      name: "exit",
+      description: "Exits the pokedex",
+      callback: commandExit,
+    },
+    help: {
+      name: "help",
+      description: "Help for pokedex",
+      callback: commandHelp,
+    },
+  };
+}
