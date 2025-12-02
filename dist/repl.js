7fe6345287e7317c09a0a42c5119b79ffedd0061
Martin-Stach <dev@martinstach.de>
2025-12-02 22:18:10 +0100

    init commit

---
+++ dist/repl.js
@@ -0,0 +1,33 @@
+import { createInterface } from "readline";
+const readline = createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: "Pokedex > ",
+});
+export function startREPL() {
+    console.log("Starting REPL");
+    readline.prompt();
+    readline.on("line", (input) => {
+        let clean = cleanInput(input);
+        if (clean.length === 0) {
+            readline.prompt();
+            return;
+        }
+        else {
+            console.log(`Your command was: ${clean[0]}`);
+        }
+        readline.prompt();
+    });
+}
+export function cleanInput(input) {
+    let split = input
+        .toLowerCase()
+        .trim()
+        .split(" ")
+        .filter((word) => word != "");
+    let result = [];
+    for (const word of split) {
+        result.push(word.trim());
+    }
+    return result;
+}
